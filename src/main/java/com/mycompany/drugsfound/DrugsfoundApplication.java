125015810e99324579a3c5060e52cc73bbcc74e3
Tchanga Dominique <dominique.tchanga@yahoo.fr>
2023-12-22 07:17:34 +0100

    first commit

---
+++ src/main/java/com/mycompany/drugsfound/DrugsfoundApplication.java
@@ -0,0 +1,14 @@
+package com.mycompany.drugsfound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugsfoundApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DrugsfoundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/drugsfound/DrugsfoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.drugsfound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugsfoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
